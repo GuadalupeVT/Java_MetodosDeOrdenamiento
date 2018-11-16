eaf2a336a1461693e104d699b78b63f1232d8cd5
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 09:51:17 -0600

    Se agrego el metodo ordenamientoBurbuja1 a la clase MetodosDeOrdenamiento en el archivo EjemploMetodosOrdenamiento.java

--- src/EjemploMetodosOrdenamiento.java
+++ src/EjemploMetodosOrdenamiento.java
@@ -24,6 +24,30 @@ class MetodosOrdenamiento{
 		System.out.println("Intercambios: "+intercambios);
 		System.out.println("Comparaciones: "+comparaciones);
 	}//Class OrdenamientoBurbuja0
+	
+	public static void ordenamientoBurbuja1(int numeros[]) {
+		long tInicio,tFin;
+		tInicio=System.nanoTime();
+		int aux, intercambios=0, recorridos=0, comparaciones=0;
+		for(int i=2; i<=numeros.length;i++) {
+			for (int j=0; j<=(numeros.length-i); j++) {
+				comparaciones++;
+				if (numeros[j]>numeros[j+1]) {
+					aux =numeros[j];
+					numeros[j]=numeros[j+1];
+					numeros[j+1]=aux;
+					intercambios++;
+				}
+				recorridos++;
+			}	
+		}
+		tFin=System.nanoTime();
+		double milliseconds = (tFin-tInicio) / 1000000.0;
+		System.out.println("-----Tiempo de procesamineto: "+(milliseconds));
+		System.out.println("Recorridos: "+recorridos);
+		System.out.println("Intercambios: "+intercambios);
+		System.out.println("Comparaciones: "+comparaciones);
+	}//OrdenamietoBurbuja1
 }
 public class EjemploMetodosOrdenamiento {
 
