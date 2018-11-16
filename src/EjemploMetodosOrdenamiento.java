4b446a6c0eea4218250a6ea71f0e4cf930fa4cc9
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 10:40:00 -0600

    Se agrego el metodo ordenamientoPorSeleccion a la clase MetodosOrdenamiento a EjemploMetodosOrdenamiento.java

--- src/EjemploMetodosOrdenamiento.java
+++ src/EjemploMetodosOrdenamiento.java
@@ -107,6 +107,37 @@ class MetodosOrdenamiento{
 		System.out.println("Comparaciones: "+comparaciones);
 	} //OrdenamientoBurbuja3
 	
+	public static void ordenamientoPorSeleccion(int []numeros) {
+		int recorridos=0, intercambios=0, comparaciones=0;
+		long tInicio,tFin;
+		tInicio=System.nanoTime();
+		 int  menor, pos, tmp;
+         for (int i = 0; i < numeros.length - 1; i++) { 
+               menor = numeros[i];
+               pos = i; 
+               for (int j = i + 1; j < numeros.length; j++){
+                     if (numeros[j] < menor) { 
+                         menor = numeros[j]; 
+                         pos = j;
+                     }
+               }
+               comparaciones++;
+               if (pos != i){ 
+                   tmp = numeros[i];
+                   numeros[i] = numeros[pos];
+                   numeros[pos] = tmp;
+                   intercambios++;
+               }
+               recorridos++;
+         }
+         tFin=System.nanoTime();
+ 		double milliseconds = (tFin-tInicio) / 1000000.0;
+ 		System.out.println("-----Tiempo de procesamineto: "+(milliseconds));
+ 		System.out.println("Recorridos: "+recorridos);
+ 		System.out.println("Intercambios: "+intercambios);
+ 		System.out.println("Comparaciones: "+comparaciones);
+	}//Metodo ordenamientoPorSeleccion
+	
 }//MetodosOrdenamiento 
 
 public class EjemploMetodosOrdenamiento {
